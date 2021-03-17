9d9cee0d83d9a15d1cec5c9d7cda400a7507f9df
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-03-17 11:43:37 +0900

    Add: Token Module 추가 및 Token Service 구현

---
+++ src/token/token.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { JWT } from 'config/dotenv';
+import { User } from 'entities/user.entity';
+import * as jwt from 'jsonwebtoken';
+
+type TokenPayloadType = {
+  id: string;
+}
+
+@Injectable()
+export class TokenService {
+  createToken(user: User): string {
+    const { id } = user;
+    const payload: TokenPayloadType = {
+      id,
+    }
+
+    const options: jwt.SignOptions = {
+      expiresIn: JWT.EXPIRES_IN,
+      issuer: JWT.ISSUER,
+      subject: JWT.SUBJECT,
+    }
+
+    return jwt.sign(payload, JWT.SECRET, options);
+  }
+
+  verifyToken(token: string) {
+    const decoded = jwt.verify(token, JWT.SECRET) as TokenPayloadType;
+
+    return decoded;
+  }
+}
