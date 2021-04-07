3efc3667b229d85fe69045b661774e2209a3a090
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-04-07 10:47:42 +0900

    Fix: Inquiry RO에 CreatedAt 추가

--- src/inquiry/ro/inquiry.ro.ts
+++ src/inquiry/ro/inquiry.ro.ts
@@ -1,16 +1,16 @@
 import { Inquiry } from 'entities/inquiry.entity';
 import { User } from 'entities/user.entity';
-import { InquiryModule } from 'inquiry/inquiry.module';
 import { LectureRo } from 'lecture/ro/lecture.ro';
 import { Builder } from 'utils/builder/builder.util';
 
 export class InquiryRo {
   content!: string;
   lecture!: LectureRo;
+  createdAt!: Date;
   user!: User | null;
 
   static fromInquiry(inquiry: Inquiry): InquiryRo {
-    const { lecture, content, user } = inquiry;
+    const { lecture, content, user, createdAt } = inquiry;
 
     const lectureRo = LectureRo.fromLecture(lecture);
 
@@ -18,6 +18,7 @@ export class InquiryRo {
       .content(content)
       .user(user)
       .lecture(lectureRo)
+      .createdAt(createdAt)
       .build();
   }
 }
